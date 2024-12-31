c9e8a00d924660d1a222492431bd2baea1b54530
Nquartey17 <niikwarteiq@gmail.com>
2024-12-31 11:15:32 -0500

    Part 3 completed with idea guidance

--- Hangman.py
+++ Hangman.py
@@ -4,14 +4,32 @@ word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-placeholder = "_" * len(chosen_word)
-print(placeholder)
-guess = input("Guess a letter: ").lower()
 
-display = ""
-for letter in chosen_word:
-    if letter == guess:
-        display += letter
-    else:
-        display += "_"
-print(display)
+word_length = len(chosen_word)
+placeholder = "_" * word_length
+print(placeholder)
+
+count = 0
+new_display = ""
+correct_letters = []
+
+while count < word_length:
+    guess = input("Guess a letter: ").lower()
+    count += 1
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(letter)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print(display)
+
+    if "_" not in display:
+        print("You win")
+        break
+
