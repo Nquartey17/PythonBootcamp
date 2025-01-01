7ca4f9a019e1a04e5586dad8a3176e59a62238cd
Nquartey17 <niikwarteiq@gmail.com>
2025-01-01 12:00:29 -0500

    CaesarCypher.py part 1 completed

---
+++ CaesarCypher/CaesarCypher.py
@@ -0,0 +1,17 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(original_text, shift_amount):
+    result_text = ""
+    for letter in original_text:
+        value = alphabet.index(letter)
+        value += shift_amount
+        if value >= 26:
+            value -= 26
+        result_text += alphabet[value]
+    print(result_text)
+
+encrypt(text, shift)
