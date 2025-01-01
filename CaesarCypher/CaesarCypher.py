6298d641a12f9f5fee5a39b4e486e11ffcb84639
Nquartey17 <niikwarteiq@gmail.com>
2025-01-01 16:06:13 -0500

    CaesarCypher.py part 2 completed

--- CaesarCypher/CaesarCypher.py
+++ CaesarCypher/CaesarCypher.py
@@ -4,14 +4,45 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(original_text, shift_amount):
+# def encrypt(original_text, shift_amount):
+#     result_text = ""
+#     for letter in original_text:
+#         shifted_position = alphabet.index(letter) + shift_amount
+#
+#         shifted_position %= len(alphabet)
+#         result_text += alphabet[shifted_position]
+#     print(result_text)
+#
+# encrypt(text, shift)
+#
+# def decrypt(original_text, shift_amount):
+#     decrypted_word = ""
+#     for letter in original_text:
+#         position_shift = alphabet.index(letter) - shift_amount
+#
+#         if position_shift < 0:
+#             position_shift += len(alphabet)
+#         decrypted_word += alphabet[position_shift]
+#     print(decrypted_word)
+
+# decrypt(text, shift)
+
+
+def caesar(original_text, shift_amount, action):
     result_text = ""
+
     for letter in original_text:
-        value = alphabet.index(letter)
-        value += shift_amount
-        if value >= 26:
-            value -= 26
-        result_text += alphabet[value]
+        shifted_position = alphabet.index(letter)
+        if action == "encode":
+            shifted_position += shift_amount
+            shifted_position %= len(alphabet)
+
+        elif action == "decode":
+            shifted_position -= shift_amount
+            if shifted_position < 0:
+                shifted_position += len(alphabet)
+
+        result_text += alphabet[shifted_position]
     print(result_text)
 
-encrypt(text, shift)
+caesar(text, shift, direction)
