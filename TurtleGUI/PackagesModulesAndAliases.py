5d146281c047a6963cd87733bb858e3cce83765d
Nquartey17 <niikwarteiq@gmail.com>
2025-06-13 12:44:23 -0400

    File for understanding packages, modules, and aliases

---
+++ TurtleGUI/PackagesModulesAndAliases.py
@@ -0,0 +1,15 @@
+# import turtle
+# tim  = turtle.Turtle()
+
+# from turtle import Turtle
+#
+# tim = Turtle()
+# tom = Turtle()
+# terry = Turtle()
+
+# import turtle as t
+# tim = t.Turtle()
+
+#pycharm can install modules through pip if there aren't already installed
+import heroes
+print(heroes.gen())
