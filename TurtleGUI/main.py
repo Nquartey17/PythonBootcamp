17724b7dcd9a2c4cbec85e13f6973eaa5cca67f2
Nquartey17 <niikwarteiq@gmail.com>
2025-06-16 22:53:29 -0400

    Part 3 completed

--- TurtleGUI/main.py
+++ TurtleGUI/main.py
@@ -6,15 +6,38 @@ turtle = Turtle()
 # turtle.color("red")
 turtle.shape("classic")
 
-# Function for turtle to move in dashed line
-def move_turtle(spaces):
-    for _ in range(spaces):
-        turtle.penup()
-        turtle.forward(10)
-        turtle.pendown()
-        turtle.forward(10)
+
+def draw_shape(color,sides):
+    turtle.color(color)
+    angle = 360 / sides
+    for _ in range(sides):
+        turtle.forward(125)
+        turtle.right(angle)
+
+#triangle
+draw_shape("red",3)
+
+#square
+draw_shape("blue",4)
+
+#pentagon
+draw_shape("green",5)
+
+#hexagon
+draw_shape("DarkGoldenrod",6)
+
+#heptagon
+draw_shape("tomato",7)
+
+#octagon
+draw_shape("black",8)
+
+#nonagon
+draw_shape("navy",9)
+
+#decagon
+draw_shape("deeppink4",10)
 
 
-move_turtle(50)
 screen = Screen()
 screen.exitonclick()
