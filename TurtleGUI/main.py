7f08ef89e998e836933fd4fe316f6748a513c3b8
Nquartey17 <niikwarteiq@gmail.com>
2025-06-24 09:21:24 -0400

    Making a random color using tuple completed

--- TurtleGUI/main.py
+++ TurtleGUI/main.py
@@ -6,8 +6,20 @@ turtle = Turtle()
 # turtle.shape("turtle")
 # turtle.color("red")
 turtle.shape("classic")
+turtle.color()
 
-colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+#screen color mode needs to be set to 255 to work
+screen = Screen()
+screen.colormode(255)
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    my_tuple = (r,g,b)
+    return my_tuple
+
+
 angles = [0,90,180,270]
 turtle.pensize(10)
 #increase/decrease animation speed
@@ -15,9 +27,9 @@ turtle.speed("fastest")
 
 #Random walk
 for _ in range(100):
-    turtle.color(random.choice(colors))
+    turtle.color(random_color())
     turtle.setheading(random.choice(angles))
     turtle.forward(50)
 
-screen = Screen()
+
 screen.exitonclick()
