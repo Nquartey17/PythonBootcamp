a1895af9764f1a968b933ab3dc73965d2e05efe0
Nquartey17 <niikwarteiq@gmail.com>
2025-06-24 11:51:57 -0400

    Made a list of common colors rgb values. removed values close to 255 because they are close the the background white color

--- HirstPainting/main.py
+++ HirstPainting/main.py
@@ -1,3 +1,17 @@
 import colorgram
 
-colors = colorgram.extract('image.jpg', 10)
+colors = colorgram.extract('image.jpg', 15)
+colors_list = []
+#
+# for i in range(len(colors)):
+#     color = colors[i]
+#     rgb = color.rgb
+#     red = rgb.r
+#     green = rgb.g
+#     blue = rgb.b
+#     rgb_color = (red,green,blue)
+#     colors_list.append(rgb_color)
+#
+# print(colors_list)
+pulled_colors = [(1, 9, 30), (121, 95, 41), (72, 32, 21), (238, 212, 72), (220, 81, 59), (226, 117, 100), (93, 1, 21), (178, 140, 170), (151, 92, 115), (35, 90, 26), (6, 154, 73)]
+
