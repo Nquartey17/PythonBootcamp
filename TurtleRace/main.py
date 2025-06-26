24d0b541ea9db8c94975b499102f7f94c7b4c905
Nquartey17 <niikwarteiq@gmail.com>
2025-06-26 10:56:00 -0400

    Simplified solution for initializing turtles

--- TurtleRace/main.py
+++ TurtleRace/main.py
@@ -1,48 +1,38 @@
-import random
 from turtle import Turtle, Screen
+import random
 
+is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
-is_race_on = False
-
-red = Turtle(shape="turtle")
-orange = Turtle(shape="turtle")
-yellow = Turtle(shape="turtle")
-green = Turtle(shape="turtle")
-blue = Turtle(shape="turtle")
-purple = Turtle(shape="turtle")
-
-all_turtles = [red, orange, yellow, green, blue, purple]
-
-def initialize_turtle(turtle,color, y):
-    turtle.penup()
-    turtle.color(color)
-    turtle.goto(x=-230, y=y)
-
-initialize_turtle(red,"red", -100)
-initialize_turtle(orange,"orange", -70)
-initialize_turtle(yellow,"yellow", -40)
-initialize_turtle(green,"green", -10)
-initialize_turtle(blue,"blue", 20)
-initialize_turtle(purple,"purple", 50)
+#Create 6 turtles
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
 
 if user_bet:
     is_race_on = True
 
-    while is_race_on:
+while is_race_on:
+    for turtle in all_turtles:
+        #230 is 250 - half the width of the turtle.
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
 
-        for turtle in all_turtles:
-            if turtle.xcor() > 230:
-                is_race_on = False
-                winning_color = turtle.pencolor()
-                if winning_color == user_bet:
-                    print(f"You've bet correctly, {winning_color} won the race")
-                else:
-                    print(f"You've bet incorrectly, {winning_color} won the race")
-
-            rand_distance = random.randint(0, 10)
-            turtle.forward(rand_distance)
+        #Make each turtle move a random amount.
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
 screen.exitonclick()
