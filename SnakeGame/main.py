9bf1f21fc33099075b636ef8c762102a4d431df8
Nquartey17 <niikwarteiq@gmail.com>
2025-06-27 11:38:36 -0400

    Implemented directional movements, prevented snake from turning in on itself

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -11,9 +11,15 @@ game_start = True
 
 snake = Snake()
 
+screen.listen() # listen for keep press
+screen.onkey(snake.up, "Up") # case-sensitive
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
 while game_start:
     screen.update()
-    time.sleep(0.1) #
+    time.sleep(0.1) # refresh screen after this duration
 
     snake.move()
 
--- SnakeGame/snake.py
+++ SnakeGame/snake.py
@@ -2,11 +2,17 @@ from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 class Snake:
 
     def __init__(self):
         self.snake_body = []
         self.create_snake()
+        self.head = self.snake_body[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -22,4 +28,21 @@ class Snake:
             new_y = self.snake_body[segment_number - 1].ycor()
             self.snake_body[segment_number].goto(new_x, new_y) # last segment
 
-        self.snake_body[0].forward(MOVE_DISTANCE) #move the head of the snake
+        self.head.forward(MOVE_DISTANCE) #move the head of the snake
+
+# prevent the snake from turning in on itself by preventing it from going in the opposite of current direction
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
