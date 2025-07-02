eed92d61e279bb6cd7af22f684e9f05ea58d53ed
Nquartey17 <niikwarteiq@gmail.com>
2025-07-02 10:10:38 -0400

    moved new_file.txt to desktop, retrieving file from there

--- ReadAndWrite/main.py
+++ ReadAndWrite/main.py
@@ -1,11 +1,9 @@
-# with open(file="my_file.txt") as file:
-#     contents = file.read()
-#     print(contents)
+with open(file="/Users/15714/Desktop/new_file.txt") as file:
+    contents = file.read()
+    print(contents)
 
-# Remember to close files if you're not using with because python might close it when more resources are needed
+with open(file="../../../Desktop/new_file.txt") as file:
+    contents = file.read()
+    print(contents)
 
-# Modes: w - write, a - add
-with open(file="new_file.txt", mode="a") as file:
-    file.write("\nNew text.")
 
-# If you try to write to a file that doesn't exist, it will be created
