e830e98118f93dd52a7f5337e6ddd881f4463739
Nquartey17 <niikwarteiq@gmail.com>
2025-07-13 17:27:39 -0400

    Learning about pandas library for csv files

---
+++ Pandas/main.py
@@ -0,0 +1,19 @@
+# import csv
+#
+# with open (file="../Pandas/weather_data.csv") as file:
+#     data = csv.reader(file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#
+#     print(temperatures)
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data)
+print()
+
+#Getting columns (pandas takes first row as column names)
+print(data["temp"])
