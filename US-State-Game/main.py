a811de60aa2020b6d1ca0dc85e73bd8576d8b334
Nquartey17 <niikwarteiq@gmail.com>
2025-07-29 16:00:59 -0400

    Partial completion of programing, need to implement while loop until all states are guessed

--- US-State-Game/main.py
+++ US-State-Game/main.py
@@ -1,10 +1,30 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+correct_states = 0
 
-answer_state = screen.textinput(title="Guess the State", prompt="Enter a state name")
+data = pandas.read_csv("50_states.csv")
+temp_list = data.state.to_list()
 
+
+answer_state = screen.textinput(f"Guess the State {correct_states}/50", "Enter a state name")
+if answer_state.capitalize() in temp_list:
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+
+    guessed_state = data[data.state == answer_state.capitalize()]
+    t.goto(guessed_state.x.item(), guessed_state.y.item())
+    t.write(guessed_state.state.item())
+
+    print(answer_state.capitalize())
+
+
+
+
+turtle.mainloop()
