296158d896193a7f55c01d420307a977a9f00816
Nquartey17 <niikwarteiq@gmail.com>
2025-08-10 13:43:51 -0400

    US state game completed, adjustments made for states with spaces in name

--- US-State-Game/main.py
+++ US-State-Game/main.py
@@ -11,21 +11,28 @@ correct_states = []
 data = pandas.read_csv("50_states.csv")
 temp_list = data.state.to_list()
 
-while correct_states < 50:
-    answer_state = screen.textinput(f"Guess the State {correct_states}/50", "Enter a state name")
-    if answer_state.capitalize() in temp_list:
+while len(correct_states) < 50:
+    answer_state = screen.textinput(f"Guess the State {len(correct_states)}/50", "Enter a state name").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in temp_list:
+            if state not in correct_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
+    if answer_state in temp_list:
         correct_states.append(answer_state)
         t = turtle.Turtle()
         t.hideturtle()
         t.penup()
 
-        guessed_state = data[data.state == answer_state.capitalize()]
+        guessed_state = data[data.state == answer_state]
         t.goto(guessed_state.x.item(), guessed_state.y.item())
         t.write(guessed_state.state.item())
 
-        print(answer_state.capitalize())
+        print(answer_state)
 
-
-
-
-turtle.mainloop()
+#states to learn csv
