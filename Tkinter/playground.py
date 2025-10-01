254fd88c842166de6607ab6b6efe3d78200f5f23
Nquartey17 <niikwarteiq@gmail.com>
2025-09-30 21:06:01 -0400

    **kwargs practice and understanding

--- Tkinter/playground.py
+++ Tkinter/playground.py
@@ -8,4 +8,24 @@ def add(*args):
         total += n
     return total
 
-print(add(1,3,5,7,4,4,6))
+# print(add(1,3,5,7,4,4,6))
+
+#Create keyword arguments to use in dictionary
+def calculate(n, **kwargs):
+    print(kwargs)
+    print(kwargs["add"]) #get value by name
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+calculate(2, add=3, multiply=5)
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make") #kw["make"] - using get prevents errors if value is empty
+        self.model = kw.get("model") # kw["model"]
+
+my_car = Car(make="Nissan", model="GT-R")
+print(my_car.model)
+my_car = Car(make="Nissan")
+print(my_car.model)
