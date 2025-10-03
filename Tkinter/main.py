d91242ff63ff0fa06276bc183c2f0e2df769d588
Nquartey17 <niikwarteiq@gmail.com>
2025-10-03 11:47:39 -0400

    Grid placement and padding

--- Tkinter/main.py
+++ Tkinter/main.py
@@ -3,88 +3,30 @@ from tkinter import *
 window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=100, pady=200) #padding
 
 #Label
-my_label = Label(text="This is a label", font=("Arial", 24))
-my_label.pack() #places on screen
-
-#Config/change properties of created component
-my_label["text"] = "New Text"
+my_label = Label(text="This is a label", font=("Arial", 24, "bold"))
 my_label.config(text="New Text")
+my_label.grid(column=0,row=0)
 
 #Buttons
-
 def button_clicked():
     # Challenge - get entry text print when button is clicked
     my_label["text"] = entry_input.get()
     print("I got clicked")
 
 button = Button(text="Click Me", command=button_clicked) #Command gets name of function, doesn't need ()
-button.pack()
+button.grid(column=1,row=1)
+
+button2 = Button(text="New button")
+button2.grid(column=2,row=0)
 
 #Entry
-
 entry_input = Entry(width=10)
-entry_input.pack()
 print(entry_input.get())
+entry_input.grid(column=3,row=2)
 
-#Text
-text = Text(height=5, width=30)
-#Puts cursor in textbox.
-text.focus()
-#Adds some text to begin with.
-text.insert(END, "Example of multi-line text entry.")
-#Get's current value in textbox at line 1, character 0
-print(text.get("1.0", END))
-text.pack()
-
-#Spinbox
-def spinbox_used():
-    #gets the current value in spinbox.
-    print(spinbox.get())
-spinbox = Spinbox(from_=0, to=10, width=5, command=spinbox_used)
-spinbox.pack()
-
-#Scale
-#Called with current scale value.
-def scale_used(value):
-    print(value)
-scale = Scale(from_=0, to=100, command=scale_used)
-scale.pack()
-
-#Checkbutton
-def checkbutton_used():
-    #Prints 1 if On button checked, otherwise 0.
-    print(checked_state.get())
-#variable to hold on to checked state, 0 is off, 1 is on.
-checked_state = IntVar()
-checkbutton = Checkbutton(text="Is On?", variable=checked_state, command=checkbutton_used)
-checked_state.get()
-checkbutton.pack()
-
-#Radiobutton
-def radio_used():
-    print(radio_state.get())
-#Variable to hold on to which radio button value is checked.
-radio_state = IntVar()
-radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
-radiobutton2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
-radiobutton1.pack()
-radiobutton2.pack()
-
-
-#Listbox
-def listbox_used(event):
-    # Gets current selection from listbox
-    print(listbox.get(listbox.curselection()))
-
-listbox = Listbox(height=4)
-fruits = ["Apple", "Pear", "Orange", "Banana"]
-for item in fruits:
-    listbox.insert(fruits.index(item), item)
-listbox.bind("<<ListboxSelect>>", listbox_used)
-listbox.pack()
-
-
+#Note - pack and grid are incompatible
 #Keeps the window open, keep at the end of the program
 window.mainloop()
