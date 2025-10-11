368ff68a9db33cd8439d285e509434368d020bed
Nquartey17 <niikwarteiq@gmail.com>
2025-10-11 19:12:57 -0400

    Live timer functionality completed

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import math
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -13,25 +15,37 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5 * 60)
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+def count_down(count):
+    count_minute = math.floor(count / 60)
+    count_seconds = count % 60
+
+    #Changing text in tkinter, input canvas variable and element you want to chage
+    canvas.itemconfig(timer_text, text=f"{count_minute}:{count_seconds}")
+
+    if count > 0:
+        # after set time in ms, function, **args input(s) to add to function
+        window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100,pady=50,bg=YELLOW)
-# fg, ✔, grid instead of pack
 
 canvas = Canvas(width=200, height=224,bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png") #Use PhotoImage for pictures in tkinter
 canvas.create_image(100, 112,image=tomato_img)
-canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
+timer_text = canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
 canvas.grid(row=1, column=1)
-
 
 timer_label = Label(text="Timer", font=(FONT_NAME, 40, "bold"), fg=GREEN, bg=YELLOW)
 timer_label.grid(row=0,column=1)
 
-start_button = Button(text="Start")
+start_button = Button(text="Start", command=start_timer)
 start_button.grid(row=2,column=0)
 
 reset_button = Button(text="Reset")
@@ -39,5 +53,7 @@ reset_button.grid(row=2,column=2)
 
 checkmark = Label(text="✔", fg=GREEN, bg=YELLOW)
 checkmark.grid(row=3, column=1)
+
+
 
 window.mainloop()
