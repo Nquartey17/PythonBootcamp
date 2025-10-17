9dcf6bdae3250536dedf64c5fc15d488de88fe21
Nquartey17 <niikwarteiq@gmail.com>
2025-10-17 13:07:05 -0400

    Values saved in seperate file, some entries cleared after password creation

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -2,7 +2,14 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-
+def save_password():
+    with open("password.txt", "a") as f:
+        f.write(f"Website: {web_box.get()}\n"
+                f"Email: {email_box.get()}\n"
+                f"Password: {password_box.get()}\n\n")
+    web_box.delete(0,END)
+    password_box.delete(0, END)
+    print("Saved")
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -26,15 +33,17 @@ password.grid(row=3, column=0)
 #Entries
 web_box = Entry(width=35)
 web_box.grid(row=1, column=1, columnspan=2, sticky="EW")
+web_box.focus() #cursor will automatically start here
 email_box = Entry(width=35)
 email_box.grid(row=2, column=1, columnspan=2, sticky="EW")
-email_box = Entry(width=21)
-email_box.grid(row=3, column=1, sticky="EW")
+email_box.insert(0,"niikwarteiq@gmail.com") #Start with commonly used email
+password_box = Entry(width=21)
+password_box.grid(row=3, column=1, sticky="EW")
 
 #Buttons
 generate_password = Button(text="Generate Password")
 generate_password.grid(row=3, column=2, columnspan=1)
-add_button = Button(width=36, text="Add")
+add_button = Button(width=36, text="Add", command=save_password)
 add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
 
 
