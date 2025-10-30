9e7fa4db9dfaf25ae511ccc22ffca6e78c22a5fa
Nquartey17 <niikwarteiq@gmail.com>
2025-10-29 21:30:58 -0400

    Raising Exceptions

--- Exceptions/main.py
+++ Exceptions/main.py
@@ -13,3 +13,14 @@ else: #when no errors are found
 finally:
     file.close()
     print("File was closed")
+
+
+#Exception raising
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human height shouldn't be over 3 meters")
+
+bmi = weight / height ** 2
+print(bmi)
