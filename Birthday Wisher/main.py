1c10efb31866105fd8f38bca4f5b54924befa12a
Nquartey17 <niikwarteiq@gmail.com>
2025-11-11 08:16:54 -0500

    Learning datetime

--- Birthday Wisher/main.py
+++ Birthday Wisher/main.py
@@ -1,13 +1,24 @@
-import smtplib
+# import smtplib
+#
+# #Using fake credentials, code will have errors
+#
+# my_email = "test@gmail.com"
+# password = "test123"
+#
+# with smtplib.SMTP("stmp.gmail.com") as connection: #Use to automatically close file after use
+#     connection.starttls() #secure connection
+#     connection.login(user=my_email, password=password) #login to email
+#     connection.sendmail(from_addr=my_email,
+#                         to_addrs="sample@yahoo.com", msg="Subject:Hello\n\nThis is the body of the email") #send mail
+#     connection.close() # close message
 
-#Using fake credentials, code will have errors
+import datetime as dt
 
-my_email = "test@gmail.com"
-password = "test123"
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday() #Counts from 0, Sunday would be 0
+print(now)
 
-with smtplib.SMTP("stmp.gmail.com") as connection: #Use to automatically close file after use
-    connection.starttls() #secure connection
-    connection.login(user=my_email, password=password) #login to email
-    connection.sendmail(from_addr=my_email,
-                        to_addrs="sample@yahoo.com", msg="Subject:Hello\n\nThis is the body of the email") #send mail
-    connection.close() # close message
+dob = dt.datetime(year=1999,month=2,day=9)
+print(dob)
