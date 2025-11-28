1636127ccb3de880d9f7c12b3ef7a0d457d4eed0
Nquartey17 <niikwarteiq@gmail.com>
2025-11-28 11:23:17 -0500

    Used API to find ISS position

--- APIs/main.py
+++ APIs/main.py
@@ -1,4 +1,18 @@
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
+response.raise_for_status() #Raise exception for error
+
+data = response.json()
+print(data)
+
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_position = (longitude, latitude)
+print(iss_position)
+
+
+# response code meanings: 1XX: processing request, 2XX: returning data, 3XX: no permission
+# 4XX: User mistake/error, 5XX: server mistake/error
+
