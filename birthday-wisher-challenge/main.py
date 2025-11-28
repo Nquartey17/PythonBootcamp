06441e26d40f3f49844626bbfaa90db3adad5d23
Nquartey17 <niikwarteiq@gmail.com>
2025-11-28 10:21:58 -0500

    Completed birthday wisher challange with guidance

---
+++ birthday-wisher-challenge/main.py
@@ -0,0 +1,29 @@
+
+from datetime import datetime
+import pandas
+import random
+import smtplib
+
+MY_EMAIL = "sample@gmail.com" #Use real email here
+MY_PASSWORD = "password" #Use real password here
+
+today = datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt" #pick random letter file
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("Use real email here") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        )
