564afb79f25ea0496114617e9532ccf8c2840402
Nquartey17 <niikwarteiq@gmail.com>
2025-12-08 19:42:54 -0500

    UI setup complete, added labels, buttons, canvas, and text

--- quizzler-app/ui.py
+++ quizzler-app/ui.py
@@ -1,2 +1,30 @@
+from tkinter import  *
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        # Labels
+        self.score = Label(text="Score: 0", background=THEME_COLOR, fg="white")
+        self.score.grid(row=0, column=1)
+
+        #Canvas
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.questions = self.canvas.create_text(150, 125, text="Text", font=("Arial", 20, "italic"))
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=30)
+
+        #Buttons (since aren't used anywhere else in the class, self.[name] isn't needed
+        true_img = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_img, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+
+        false_img = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_img, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
+
+        self.window.mainloop()
