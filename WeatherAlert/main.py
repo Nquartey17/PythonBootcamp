2a0bb9e95345107e40addfa0ff5b0fdba9ea0a23
Nquartey17 <niikwarteiq@gmail.com>
2026-01-01 14:33:36 -0500

    Notify user every 3 hours if weather code is less than 700

--- WeatherAlert/main.py
+++ WeatherAlert/main.py
@@ -1,17 +1,24 @@
 import requests
-import os
 
-api_key = os.getenv('owm_api_key')
+
+api_key = "INSERT_API_KEY_HERE"
 api = "https://api.openweathermap.org/data/2.5/weather?q=Woodbridge,VA,US&appid=f6da2c90c159c35437fa837e6f7b4869"
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 
 LAT = 37.431572
 LONG = -78.656891
+API_RAIN_CODE = 700
 
 weather_parameters = {
     "lat": LAT,
     "lon": LONG,
-    "appid": api_key
+    "appid": api_key,
+    "cnt": 4
 }
 response = requests.get(OWM_Endpoint, params=weather_parameters)
-print(response.json())
+response.raise_for_status()
+weather_data = response.json()
+# print(weather_data["list"][0]["weather"][0]["id"])
+for hours in weather_data["list"]:
+    if int(hours["weather"][0]["id"]) < API_RAIN_CODE:
+        print("It's raining, bring an umbrella")
