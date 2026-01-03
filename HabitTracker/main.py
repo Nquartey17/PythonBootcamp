17657f4797b09bfab6100d3ab81c0f49c113696e
Nquartey17 <niikwarteiq@gmail.com>
2026-01-03 13:06:53 -0500

    Creating a 5k graph in pixela

--- HabitTracker/main.py
+++ HabitTracker/main.py
@@ -1,19 +1,38 @@
 import os
-
 import requests
 from dotenv import load_dotenv
 
+# view graph here -> "https://pixe.la/v1/users/nquartey/graphs/graph1.html"
+USERNAME = "nquartey"
+
 pixela_endpoint = "https://pixe.la/v1/users"
 load_dotenv()
-token_key = os.getenv("token")
+TOKEN_KEY = os.getenv("token")
 
-#Create a new pixela account
 user_params = {
-    "token": token_key,
-    "username": "nquartey",
+    "token": TOKEN_KEY,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
+#Create a new pixela account
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
+
+graph_endpoint =f"{pixela_endpoint}/{USERNAME}/graphs"
+graph_config = {
+    "id": "graph1",
+    "name": "5k graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "sora"
+}
+
+#Create a header so token can be used to authenticate user
+headers = {
+    "X-USER-TOKEN": TOKEN_KEY
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+print(response.text)
