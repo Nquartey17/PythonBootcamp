9c0d587d5cd9a6ccda6c59f547e197e63ad90253
Nquartey17 <niikwarteiq@gmail.com>
2026-01-20 20:14:10 -0500

    Returning article name and link with the most upvotes using beautifulsoup

--- bs4-start/main.py
+++ bs4-start/main.py
@@ -1,30 +1,68 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open("website.html") as file:
-    contents = file.read()
+response = requests.get("https://news.ycombinator.com/news")
+yc_web_page = response.text
 
-soup = BeautifulSoup(contents, "html.parser")
-# print(soup.title)
-# print(soup.title.name)
-# print(soup.title.string)
-# print(soup.prettify())
-all_anchor_tags = soup.find_all(name="a")
-print(all_anchor_tags)
+soup = BeautifulSoup(yc_web_page, "html.parser")
+articles = soup.find_all(name="span", class_="titleline")
+article_texts = []
+article_links = []
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.select_one(".titleline a")["href"]
+    article_links.append(link)
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
 
-for tag in all_anchor_tags:
-    # print(tag.getText())
-    print(tag.get(("href")))
+print(article_texts)
+print(article_links)
+print(article_upvotes)
 
-heading = soup.find(name="h1", id="name")
-print(heading)
+max_upvotes = max(article_upvotes)
+max_upvotes_index = article_upvotes.index(max_upvotes)
 
-section_heading = soup.find(name="h3",class_="heading") #class is a keyword in python. Use class_ instead
+#Article with most upvotes
+print(article_texts[max_upvotes_index])
+print(article_links[max_upvotes_index])
 
-company_url = soup.select_one(selector="p a") #get link in <p><a>
-print(company_url)
 
-name = soup.select_one(selector="#name")
-print(name)
 
-headings = soup.select(".heading") #select returns list
-print(headings)
+
+
+
+
+
+
+
+
+
+
+# with open("website.html") as file:
+#     contents = file.read()
+#
+# soup = BeautifulSoup(contents, "html.parser")
+# # print(soup.title)
+# # print(soup.title.name)
+# # print(soup.title.string)
+# # print(soup.prettify())
+# all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
+#
+# for tag in all_anchor_tags:
+#     # print(tag.getText())
+#     print(tag.get(("href")))
+#
+# heading = soup.find(name="h1", id="name")
+# print(heading)
+#
+# section_heading = soup.find(name="h3",class_="heading") #class is a keyword in python. Use class_ instead
+#
+# company_url = soup.select_one(selector="p a") #get link in <p><a>
+# print(company_url)
+#
+# name = soup.select_one(selector="#name")
+# print(name)
+#
+# headings = soup.select(".heading") #select returns list
+# print(headings)
