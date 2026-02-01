eebb1ee0485bdd74ac2e4a44f2cc39c28be9149c
Nquartey17 <niikwarteiq@gmail.com>
2026-02-01 16:16:00 -0500

    Starting Spotify project, gathered the names for top 100 songs on the entered date

---
+++ SpotifyBillboard100/main.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Billboard pro needed to see previous years on website
+year = input("What year do you want to travel to? Enter format as YYYY-MM-DD: ")
+
+url = "https://www.billboard.com/charts/hot-100/" + year
+headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"}
+
+response = requests.get(url, headers)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+songs = soup.select("li ul li h3")
+song_names = [song.getText().strip() for song in songs]
+print(song_names)
+
