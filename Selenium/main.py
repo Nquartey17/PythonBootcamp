cda54726c8941e1872968d14e728c1e4431a2f10
Nquartey17 <niikwarteiq@gmail.com>
2026-02-10 12:20:25 -0500

    Getting dates and times from upcoming events section on python website and returning as a nested dictionary

--- Selenium/main.py
+++ Selenium/main.py
@@ -24,5 +24,16 @@ print(documentation_link.text)
 bug_link = driver.find_element(By.XPATH, value='//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
 print(bug_link.text)
 
+event_times = driver.find_elements(By.CSS_SELECTOR, value=".event-widget time")
+event_names = driver.find_elements(By.CSS_SELECTOR, value=".event-widget li a")
+events_dict = {
+    n: {
+        "time": event_times[n].text,
+        "name": event_names[n].text
+    }
+    for n in range(len(event_times))
+}
+print(events_dict)
+
 #.close() will close tab, .quit() will close entire webpage
 driver.quit()
