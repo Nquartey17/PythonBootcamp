c3dadc24f0a53e2f844ab29c4593b6ad15ed587f
Nquartey17 <niikwarteiq@gmail.com>
2026-02-10 11:42:00 -0500

    Using Selenium to get different HTML elements faster than bs4

--- Selenium/main.py
+++ Selenium/main.py
@@ -1,11 +1,28 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 #Keep Chrome running after program finishes
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.amazon.com")
+driver.get("https://www.python.org")
+
+#Subject to change by Amazon, check class name if you encounter errors
+# price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
+# price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
+# print(f"The price is {price_dollar.text}.{price_cents.text}")
+
+search_bar = driver.find_element(By.NAME, value="q")
+print(search_bar.get_attribute("placeholder")) #Print placeholder in search bar
+button = driver.find_element(By.ID, value="submit")
+print(button.size)
+documentation_link = driver.find_element(By.CSS_SELECTOR, value=".documentation-widget a")
+print(documentation_link.text)
+
+#Xpath is used like finding files in windows directory, copy from webpage
+bug_link = driver.find_element(By.XPATH, value='//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
+print(bug_link.text)
 
 #.close() will close tab, .quit() will close entire webpage
 driver.quit()
