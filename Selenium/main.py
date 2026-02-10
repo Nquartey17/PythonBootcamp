099b53f1f3d02dfab9ed33505d2c4188e935d8b7
Nquartey17 <niikwarteiq@gmail.com>
2026-02-10 10:55:12 -0500

    Running webpage using Selenium

---
+++ Selenium/main.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+#Keep Chrome running after program finishes
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.amazon.com")
+
+#.close() will close tab, .quit() will close entire webpage
+driver.quit()
