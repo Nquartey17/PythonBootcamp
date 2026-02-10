bda1b7c4f6744a9a59d9073b0fe09c4e6ece94e7
Nquartey17 <niikwarteiq@gmail.com>
2026-02-10 09:23:04 -0500

    Adding headers to get request

--- AmazonPriceChecker/main.py
+++ AmazonPriceChecker/main.py
@@ -11,7 +11,20 @@ url = "https://appbrewery.github.io/instant_pot/"
 #Live URL: Note - Subject to change. If bs4 fails, check Amazon for changes
 # url = "https://www.amazon.com/Apple-Bluetooth-Headphones-Personalized-Effortless/dp/B0DGHMNQ5Z/ref=sr_1_1_sspa?crid=1BWIQLE3HMU2M&dib=eyJ2IjoiMSJ9.CI7bjSuQSQBx0GaAVvRN7sZKdmIeg5NZU-hRqMqF9GB2hJWy1j6bnla3VymYHYHxTErLINZWo6ABC5COoYdbvp8g0AmU5n2pvZ7gbIIlRmnDEWZSOH1-ytoJT3dP6v543QtsvEjKM8H_i5TKmh0LmgM-Zlf5DNXg0_2J-uuQx9fg4gnyFFZwitqgrFwd8TQDl8VsCjkSCGGkkv7NLCT_DWpPjGXunGWMYKZCnnzo4qQ.PiFPkKX7VOwKzidix49rwzxrOJXf8EWk_piR7ekCHZk&dib_tag=se&keywords=airpods&qid=1770077624&sprefix=airpods%2Bpro%2B2%2Caps%2C403&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1"
 
-response = requests.get(url)
+response = requests.get(url, headers={"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                                      "Accept-Encoding": "gzip, deflate, br, zstd",
+                                      "Accept-Language": "en-US,en;q=0.5",
+                                      "Priority": "u=0, i",
+                                      "Sec-Ch-Ua": "\"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"144\", \"Google Chrome\";v=\"144\"",
+                                      "Sec-Ch-Ua-Mobile": "?0",
+                                      "Sec-Ch-Ua-Platform": "\"Windows\"",
+                                      "Sec-Fetch-Dest": "document",
+                                      "Sec-Fetch-Mode": "navigate",
+                                      "Sec-Fetch-Site": "none",
+                                      "Sec-Fetch-User": "?1",
+                                      "Upgrade-Insecure-Requests": "1",
+                                      "User-Agent": "CCBot/2.0 (https://commoncrawl.org/faq/)"
+                                      })
 website_html = response.text
 
 soup = BeautifulSoup(website_html, "html.parser")
