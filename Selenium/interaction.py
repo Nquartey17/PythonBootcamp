c49cb21fa68b5c7e74d29089ec6a63b269908de7
Nquartey17 <niikwarteiq@gmail.com>
2026-02-11 09:03:16 -0500

    Clicking and sending inputs to web page

--- Selenium/interaction.py
+++ Selenium/interaction.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 #Keep Chrome running after program finishes
 chrome_options = webdriver.ChromeOptions()
@@ -9,6 +10,15 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
 article_num = driver.find_element(By.CSS_SELECTOR, value='#articlecount a')
-print(article_num.text)
+#click on link with selenium
+# article_num.click()
 
-driver.quit()
+#Find element by link text
+all_portals = driver.find_element(By.LINK_TEXT, "Content portals")
+# all_portals.click()
+
+#Input text in search bar
+search = driver.find_element(By.NAME, value="search")
+driver.maximize_window() #Maximize screen so text can appear in search bar
+
+search.send_keys("Python",Keys.ENTER)
