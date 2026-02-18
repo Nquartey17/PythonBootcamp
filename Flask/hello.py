c8ede92418a7565993ff8ccc2719a8ed71694950
Nquartey17 <niikwarteiq@gmail.com>
2026-02-18 09:30:44 -0500

    Applying decorator function knowledge to help print new next at different webpage location

--- Flask/hello.py
+++ Flask/hello.py
@@ -2,9 +2,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/") # hello_world will activate if user tries to react home page (/)
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/bye")
+def say_bye():
+    return "Bye"
 
 if __name__ == "__main__":
     app.run(debug=True)
