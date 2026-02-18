bec43cd563c01a4bfbba6eca4b0aae1f9ab50b85
Nquartey17 <niikwarteiq@gmail.com>
2026-02-18 13:41:13 -0500

    Advanced decorator functions learned with args and kwargs

--- Flask/DecoratorFunction.py
+++ Flask/DecoratorFunction.py
@@ -2,7 +2,7 @@ import time
 
 def delay_decorator(function):
     def wrapper_function():
-        time.sleep(2) #Activated before print since it's before function()
+        time.sleep(1) #Activated before print since it's before function()
         function()
         function()
     return wrapper_function
@@ -24,3 +24,23 @@ say_hello()
 # Alternative to activate
 decorated_function = delay_decorator(say_greeting)
 decorated_function()
+
+#Advanced decorators
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.is_logged_in = False
+
+def is_authenticated_decorator(function):
+    def wrapper(*args, **kwargs): # Allows multiple arguments besides function
+        if args[0].is_logged_in: # Takes first positional argument (user) as [0]
+            function(args[0])
+    return wrapper
+
+@is_authenticated_decorator
+def create_blog_pose(user):
+    print(f"This is {user.name}'s new blog post")
+
+new_user = User("Nii-Kwartei")
+new_user.is_logged_in = True
+create_blog_pose(new_user)
