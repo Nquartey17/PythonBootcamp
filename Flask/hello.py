d514c2ef08f7b16e58b18ba41444806ada332e4e
Nquartey17 <niikwarteiq@gmail.com>
2026-02-18 10:43:09 -0500

    Learning about converters, URL paths, and debugger

--- Flask/hello.py
+++ Flask/hello.py
@@ -10,5 +10,12 @@ def hello_world():
 def say_bye():
     return "Bye"
 
+# Using <> to make variable name into url
+# Converter (<converter:variable name) will specify the type of the argument
+# path works like string but also accepts slashes. String does not
+@app.route("/<path:name>/<int:number>")
+def greet(name, number):
+    return f"Hello {name}: Your number is {number}!"
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) # debug=True: Changes will reflect without having to stop code
