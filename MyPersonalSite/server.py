97b635f05414b6f4a01ff895fb603589e8790a8a
Nquartey17 <niikwarteiq@gmail.com>
2026-02-19 10:45:23 -0500

    Rendering HTML pages with Flask

---
+++ MyPersonalSite/server.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/") # hello_world will activate if user tries to react home page (/)
+def home():
+    return render_template("PersonalSite.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
