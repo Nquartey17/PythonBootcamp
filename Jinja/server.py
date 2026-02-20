baf8e0a4e71256c54f8e46dbb08f433790f97b95
Nquartey17 <niikwarteiq@gmail.com>
2026-02-20 14:04:59 -0500

    Guessing age and gender from name added (/guess/{name} using agify and genderize APIs

--- Jinja/server.py
+++ Jinja/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import random
 from datetime import date
+import requests
 
 app = Flask(__name__)
 
@@ -10,6 +11,16 @@ def home():
     year = date.today().year
     # Specify a variable name in the parameter to send to HTML file
     return render_template("index.html", num=random_number,year=year)
+
+@app.route("/guess/<path:name>")
+def guess(name):
+    age_response = requests.get(f"https://api.agify.io?name={name}")
+    age_data = age_response.json()
+
+    gender_response = requests.get(f"https://api.genderize.io?name={name}")
+    gender_data = gender_response.json()
+
+    return render_template("guess.html", name=name, age=age_data["age"], gender=gender_data["gender"])
 
 if __name__ == '__main__':
     app.run(debug=True)
