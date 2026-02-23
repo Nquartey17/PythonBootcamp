9264734028dd210616d08cb4b74026418277eaac
Nquartey17 <niikwarteiq@gmail.com>
2026-02-23 10:04:28 -0500

    Using if statements and for loops in Jinja

--- Jinja/server.py
+++ Jinja/server.py
@@ -22,6 +22,14 @@ def guess(name):
 
     return render_template("guess.html", name=name, age=age_data["age"], gender=gender_data["gender"])
 
+
+@app.route("/blog")
+def blog():
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html",posts=all_posts)
+
 if __name__ == '__main__':
     app.run(debug=True)
 
