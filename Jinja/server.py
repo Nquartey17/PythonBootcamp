e2d684b8cd780c5d56e862501351540ba13e59f9
Nquartey17 <niikwarteiq@gmail.com>
2026-02-23 10:10:25 -0500

    Creating hyperlinks to function in html file. Passing back parameters too

--- Jinja/server.py
+++ Jinja/server.py
@@ -23,8 +23,9 @@ def guess(name):
     return render_template("guess.html", name=name, age=age_data["age"], gender=gender_data["gender"])
 
 
-@app.route("/blog")
-def blog():
+@app.route("/blog/<num>")
+def get_blog(num):
+    print(num)
     blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
     response = requests.get(blog_url)
     all_posts = response.json()
