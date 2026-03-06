a56c592b7eeef56285554d77777700f2409905cc
Nquartey17 <niikwarteiq@gmail.com>
2026-03-06 09:26:54 -0500

    Sending email from user inputs. current code will produce errors unless valid email and password information is input

--- UpgradedBlog/main.py
+++ UpgradedBlog/main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request
 import requests
-
+import smtplib
 
 app = Flask(__name__)
 response = requests.get(url="https://api.npoint.io/2635a81c7b36b9705a5d").json()
+MY_EMAIL = "INPUT EMAIL HERE"
+MY_PASSWORD = "INPUT PASSWORD HERE"
 
 @app.route('/')
 def home():
@@ -16,6 +18,20 @@ def about():
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
+        # Gathering user input values
+        user_name = request.form["name"]
+        user_email = request.form["name"]
+        user_phone_number = request.form["name"]
+        user_text = request.form["name"]
+
+        with smtplib.SMTP("stmp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            connection.sendmail(from_addr=MY_EMAIL,
+                                to_addrs=MY_EMAIL,
+                                msg=f"Subject:New Message\n\nName: {user_name}\nEmail: {user_email}\nPhone: {user_phone_number}\nMessage:{user_text}")
+
+
         return render_template("contact.html", msg_sent=True)
     return render_template("contact.html", msg_sent=False)
 
