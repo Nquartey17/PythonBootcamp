a0b5196f6ce76f369532065424dcf86274d417ba
Nquartey17 <niikwarteiq@gmail.com>
2026-03-06 10:35:58 -0500

    Using Flask WTF to create a form, secret key removed from file temporarily

--- FlaskWTF/main.py
+++ FlaskWTF/main.py
@@ -1,16 +1,27 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.fields.simple import SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+#Secret key is required, randomly generate it yourself
+app.config['SECRET_KEY'] = ''
 
+class LoginForm(FlaskForm):
+    email = StringField('Email')
+    password = StringField('Password')
+    submit = SubmitField('Log In')
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET','POST'])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    return render_template("login.html",form=form)
 
 
 if __name__ == '__main__':
